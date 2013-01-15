8bd9f9288b79eaca811a14bfe43693883ed53f80
Ash Tewari <ash.tewari.name@gmail.com>
2013-01-15 07:53:19 -0500

    LoadButton, ClearButton

--- CensusMapper.ViewModels/MainViewModel.cs
+++ CensusMapper.ViewModels/MainViewModel.cs
@@ -8,12 +8,31 @@ namespace CensusMapper.ViewModels
     {
         private MapViewModel _mapViewModel;
         private ICommand _searchCommand;
+        private ICommand _clearCommand;
+        private ICommand _refreshCommand;
 
         public MainViewModel(IBingMapsApi bingMapsApi, ICensusApi censusApi)
         {
             _mapViewModel = new MapViewModel(bingMapsApi, censusApi);
 
             _searchCommand = new RelayCommand<string>(DoSearch, CanDoSearch);
+            _refreshCommand = new RelayCommand(DoRefresh);
+            _clearCommand = new RelayCommand(DoClear, CanDoClear);
+        }
+
+        private bool CanDoClear()
+        {
+            return this.MapViewModel.Items.Count > 0;
+        }
+
+        private void DoClear()
+        {
+            this.MapViewModel.Items.Clear();
+        }
+
+        private void DoRefresh()
+        {
+            this.MapViewModel.LoadStateData();
         }
 
         public MapViewModel MapViewModel
@@ -23,6 +42,26 @@ namespace CensusMapper.ViewModels
             {
                 _mapViewModel = value;
                 RaisePropertyChanged("MapViewModel");
+            }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get { return _refreshCommand; }
+            set
+            {
+                _refreshCommand = value;
+                RaisePropertyChanged("RefreshCommand");
+            }
+        }
+
+        public ICommand ClearCommand
+        {
+            get { return _clearCommand; }
+            set
+            {
+                _clearCommand = value;
+                RaisePropertyChanged("ClearCommand");
             }
         }
 
