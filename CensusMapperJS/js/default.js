c697047303e778f9da3d03056d1882eeabefc6bd
Ash Tewari <ash.tewari.name@gmail.com>
2013-04-17 23:04:27 -0400

    click to show zip code population

--- CensusMapperJS/js/default.js
+++ CensusMapperJS/js/default.js
@@ -32,12 +32,45 @@
 
     app.start();
     
+    function showerror(err) {
+        var errMsgDlg = new Windows.UI.Popups.MessageDialog(err.message);
+        errMsgDlg.showAsync();
+    }
+    
+    function getPopulationForZipCode(censusKey, postalCode, stateCode, location) {
+        var zcOptions = {
+            url: "http://api.census.gov/data/2010/sf1?key=" + censusKey + "&get=P0010001&for=zip+code+tabulation+area:" + postalCode + "&in=state:" + stateCode,
+            type: "GET",
+        };
+
+        WinJS.xhr(zcOptions).done(
+            function zcSuccess(zvReq) {
+                console.log(zvReq);
+                var pop = JSON.parse(zvReq.responseText);
+                addPushpin(location, postalCode, pop[1][0]);
+            },
+            function zcError(zcErr) {
+                showerror(zcErr.message);
+            });
+    }
+    
+    function addPushpin(location, label, number) {
+        var pushpinOptions = { width: null, height: null, htmlContent: "<div class='population-box'><div class=state-name>" + label + "</div><div>" + number + "</div></div>" };
+        var pushpin = new Microsoft.Maps.Pushpin(location, pushpinOptions);
+        map.entities.push(pushpin);
+    }
+
     function initialize() {
         // init bing maps
         Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: 'en-us', homeRegion: 'US' });
 
         //hook button clicks
         WinJS.UI.processAll().done(function () {
+            var cmdDelete = document.getElementById("cmdDelete");
+            cmdDelete.addEventListener("click", function() {
+                map.entities.clear();
+            });            
+            
             var cmdRefresh = document.getElementById("cmdRefresh");
             cmdRefresh.addEventListener("click", function() {
                 // get census data and display it              
@@ -67,17 +100,15 @@
                                             console.log(data[i][0] + " " + data[i][1]);
                                             for (var j = 0; j < states.length; j++) {
                                                 if (data[i][1] == states[j].id) {
-                                                    console.log(states[j].name);                                                    
-                                                    var pushpinOptions = { width: null, height: null, htmlContent: "<div class='population-box'><div class=state-name>" + states[j].name + "</div><div>" + data[i][0] + "</div></div>" };
-                                                    var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(states[j].lat, states[j].lng), pushpinOptions);
-                                                    map.entities.push(pushpin);
+                                                    console.log(states[j].name);
+                                                    var location = new Microsoft.Maps.Location(states[j].lat, states[j].lng);
+                                                    addPushpin(location, states[j].name, data[i][0]);
                                                 }
                                             }
                                         }
                                     },
                                     function error(err) {
-                                        var md = new Windows.UI.Popups.MessageDialog(err.message);
-                                        md.showAsync();
+                                        showerror(err);
                                     }
                                 );
                             });
@@ -87,8 +118,8 @@
             });
         });
     }
-    document.addEventListener("DOMContentLoaded", initialize, false);
-    
+    document.addEventListener("DOMContentLoaded", initialize, false);    
+
     var map;
     function initMap() {
         try {
@@ -107,12 +138,53 @@
                         zoom: 5
                     };
                     map = new Microsoft.Maps.Map(document.getElementById("mapdiv"), mapOptions);
+                    
+                    Microsoft.Maps.Events.addHandler(map, "click", function(args) {
+                        if (args.targetType == "map") {
+                            var point = new Microsoft.Maps.Point(args.getX(), args.getY());
+                            var loc = args.target.tryPixelToLocation(point);
+                            var location = new Microsoft.Maps.Location(loc.latitude, loc.longitude);
+
+                            var query = loc.latitude + "," + loc.longitude;
+                            var options = {
+                                url: 'http://dev.virtualearth.net/REST/v1/Locations/' + query + '?output=json&key=' + bingKey,                         
+                                type: "GET",
+                            };
+
+                            folder.getFileAsync("\data\\states-data.json").done(function(statesfile) {
+                                Windows.Storage.FileIO.readTextAsync(statesfile).done(function(statesData) {
+                                    console.log(statesData);
+                                    var states = JSON.parse(statesData);
+                                
+                                    WinJS.xhr(options).done(
+                                        function success(req) {
+                                            var data = JSON.parse(req.responseText);
+                                            var address = data.resourceSets[0].resources[0].address;
+                                            var postalCode = address.postalCode;
+                                            var stateAbr = address.adminDistrict;                                    
+                                            var censusKey = JSON.parse(text).Census;
+                                            
+                                            for (var j = 0; j < states.length; j++) {
+                                                if (stateAbr == states[j].abr) {
+                                                    console.log(states[j].name);
+                                                    getPopulationForZipCode(censusKey, postalCode, states[j].id, location);
+                                                    break;
+                                                }
+                                            }                                            
+                                        },
+                                        function error(err) {
+                                            showerror(err.message);
+                                        }
+                                    );
+                                });
+                            });
+                        }
+                    });
                 });
             });
         }
         catch (e) {
-            var md = new Windows.UI.Popups.MessageDialog(e.message);
-            md.showAsync();
+            showerror(e.message);
         }
     }
 })();
