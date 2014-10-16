436028e04a4df4e9f9829680b7879047ebc9c9bc
Ash Tewari <ash.tewari.name@gmail.com>
2014-10-15 23:36:46 -0400

    Custom markers

--- CMX/Android/MapViewRenderer.cs
+++ CMX/Android/MapViewRenderer.cs
@@ -7,6 +7,8 @@ using Xamarin.Forms.Maps;
 using Android.Content;
 using Android.Graphics;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 [assembly: ExportRenderer (typeof(CensusMapper.CensusMap), typeof(CensusMapper.Android.MapViewRenderer))]
 namespace CensusMapper.Android
@@ -14,7 +16,7 @@ namespace CensusMapper.Android
 	public class MapViewRenderer : MapRenderer
 	{
 
-		bool _isDrawnDone;
+		bool _isDrawnDone = false;
 
 		CensusMap _customMap { get { return this.Element as CensusMap; } }
 
@@ -37,32 +39,66 @@ namespace CensusMapper.Android
 				//The footer overlays the zoom controls
 				androidMapView.Map.UiSettings.ZoomControlsEnabled = true;
 
-//				IList<Pin> formsPins = _customMap.Pins;
-//
-//				//IList<Pin> formsPins = new List<Pin> ();
-//				//foreach (var formsPin in formsPins) {
-//				//	formsPins.Add (formsPin);
-//				//}
-//
-//				//_customMap.Pins.Clear();
-//
-//				foreach (var formsPin in formsPins) {
-//					var markerWithIcon = new MarkerOptions ();
-//
-//					markerWithIcon.SetPosition (new LatLng (formsPin.Position.Latitude, formsPin.Position.Longitude));
-//					//markerWithIcon.SetTitle (formsPin.Label);
-//					//markerWithIcon.SetSnippet (formsPin.Address);
-//
-////					if (!string.IsNullOrEmpty ("Logo"))
-////						markerWithIcon.InvokeIcon (BitmapDescriptorFactory.FromAsset (String.Format ("{0}.png", "Logo")));
-////					else
-////						markerWithIcon.InvokeIcon (BitmapDescriptorFactory.DefaultMarker ());
-//
-//					markerWithIcon.InvokeIcon (CreateCensusMarker(formsPin));
-//					androidMapView.Map.AddMarker (markerWithIcon);
-//				}
+				INotifyCollectionChanged notifyCollectionChanged = _customMap.Pins as INotifyCollectionChanged;
+				if (notifyCollectionChanged != null) {
+					notifyCollectionChanged.CollectionChanged += (new NotifyCollectionChangedEventHandler (this.OnCollectionChanged));
+				}													
+
+				//				IList<Pin> formsPins = _customMap.Pins;
+				//
+				//				IList<Pin> formsPins = new List<Pin> ();
+				//				foreach (var formsPin in _customMap.Pins) {
+				//					formsPins.Add (formsPin);
+				//				}			
+				//				_customMap.Pins.Clear();
+				//
 
 				_isDrawnDone = true;
+			}
+		}
+
+		IList<MarkerOptions> markers = new List<MarkerOptions>();
+
+		private void OnCollectionChanged (object sender, NotifyCollectionChangedEventArgs args)
+		{
+			var androidMapView = (MapView)Control;
+
+			foreach (Pin formsPin in args.NewItems) {
+				var markerWithIcon = new MarkerOptions ();
+
+				markerWithIcon.SetPosition (new LatLng (formsPin.Position.Latitude, formsPin.Position.Longitude));
+				//markerWithIcon.SetTitle (formsPin.Label);
+				//markerWithIcon.SetSnippet (formsPin.Address);
+
+				//					if (!string.IsNullOrEmpty ("Logo"))
+				//						markerWithIcon.InvokeIcon (BitmapDescriptorFactory.FromAsset (String.Format ("{0}.png", "Logo")));
+				//					else
+				//						markerWithIcon.InvokeIcon (BitmapDescriptorFactory.DefaultMarker ());
+
+				markerWithIcon.InvokeIcon (CreateCensusMarker(formsPin));
+				markers.Add (markerWithIcon);
+
+				//androidMapView.Map.AddMarker(markerWithIcon);
+
+			}
+
+						androidMapView.Map.Clear ();
+						foreach (MarkerOptions marker in markers) {
+							androidMapView.Map.AddMarker(marker);
+						}
+
+		}
+
+		protected override void OnLayout(bool changed, int l, int t, int r, int b)
+		{
+			base.OnLayout(changed, l, t, r, b);
+
+			var targetElement = (MapView)Control;
+
+			//NOTIFY CHANGE
+			if (changed)
+			{
+				_isDrawnDone = false;
 			}
 		}
 
@@ -89,7 +125,7 @@ namespace CensusMapper.Android
 
 			return BitmapDescriptorFactory.FromBitmap (bmp);
 		}
-			
+
 
 		void HandleMapClick (object sender, GoogleMap.MapClickEventArgs e)
 		{
