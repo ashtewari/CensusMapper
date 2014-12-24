5e1f98a3bd793c1f752acf3039e0c0a783b1b6c4
Ash Tewari <ash.tewari.name@gmail.com>
2014-12-24 18:21:45 -0500

    renamed to ApiService

---
+++ CMX/CensusMapper.Core.Pcl/Services/ApiService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CensusMapper
+{
+	public class ApiKeyService
+	{
+		private const string censusApiKey = "11174******";
+		private const string bingMapsKey = "AjFaZ******";
+
+		public ApiKeyService ()
+		{
+		}
+
+		public string CensusKey {
+			get
+			{
+				return censusApiKey;
+			}
+		}
+
+		public string BingMapsKey {
+			get
+			{
+				return bingMapsKey;
+			}
+		}
+	}
+}
+
