cf87e00855c880bd611187b8c350f5a2812a162f
ashtewari <mailbox@ashtewari.com>
2015-01-19 15:41:13 -0500

    CensusMapper.iOS

---
+++ CMX/CensusMapperIos/MainViewController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreLocation;
+using MonoTouch.MapKit;
+
+namespace CensusMapperIos
+{
+	public partial class MainViewController : UIViewController
+	{
+		public MainViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			CLLocationCoordinate2D coords = new CLLocationCoordinate2D(39.828127, -98.579404);
+			MKCoordinateSpan span = new MKCoordinateSpan(MilesToLatitudeDegrees(200), MilesToLongitudeDegrees(200, coords.Latitude));
+			mapView.Region = new MKCoordinateRegion(coords, span);
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+
+		/// <summary>Converts miles to latitude degrees</summary>
+		public double MilesToLatitudeDegrees(double miles)
+		{
+			double earthRadius = 3960.0; // in miles
+			double radiansToDegrees = 180.0/Math.PI;
+			return (miles/earthRadius) * radiansToDegrees;
+		}
+
+		/// <summary>Converts miles to longitudinal degrees at a specified latitude</summary>
+		public double MilesToLongitudeDegrees(double miles, double atLatitude)
+		{
+			double earthRadius = 3960.0; // in miles
+			double degreesToRadians = Math.PI/180.0;
+			double radiansToDegrees = 180.0/Math.PI;
+			// derive the earth's radius at that point in latitude
+			double radiusAtLatitude = earthRadius * Math.Cos(atLatitude * degreesToRadians);
+			return (miles / radiusAtLatitude) * radiansToDegrees;
+		}
+	}
+}
+
